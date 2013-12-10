c60a2057b1d29b4b3d10d7f82daac40cdce89bb6
kellyvigil <andreavigil@128-223-223-200-wireless.uoregon.edu>
2013-12-09 18:25:23 -0800

    move javascript

--- SF Map.js
+++ SF Map.js
@@ -1,3 +1,18 @@
+$(document).ready(function() {
+ var tag = "goducks";
+var url = "https://api.instagram.com/v1/tags/"+tag+"/media/recent?client_id=bd5b06bec64b41f9a9d75aba40c05962&callback=?"
+
+$.getJSON(url).success(function(response) {
+    var photos = response.data;
+    $.each(photos, function(index, photo) {
+        var photo_el = "<div class='ig'><a href='" + photo.link + "'><img src='" + photo.images.standard_resolution.url + "' /><p>" + photo.caption.text + "</p></a></div>";
+        $('.main').append(photo_el);
+        console.log(photo);
+  });
+}).error(function(error) {
+    alert(error);
+});
+
 function initialize() {
       var mapOptions = {
         zoom: 13,
@@ -67,20 +82,5 @@ function initialize() {
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
-
-$(document).ready(function() {
- var tag = "goducks";
-var url = "https://api.instagram.com/v1/tags/"+tag+"/media/recent?client_id=bd5b06bec64b41f9a9d75aba40c05962&callback=?"
-
-$.getJSON(url).success(function(response) {
-    var photos = response.data;
-    $.each(photos, function(index, photo) {
-        var photo_el = "<div class='ig'><a href='" + photo.link + "'><img src='" + photo.images.standard_resolution.url + "' /><p>" + photo.caption.text + "</p></a></div>";
-        $('.main').append(photo_el);
-        console.log(photo);
-  });
-}).error(function(error) {
-    alert(error);
-});
  
 });
